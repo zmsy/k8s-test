5fd15321c54c893e6dbb6a65f7ba7401356b3f6c
Zach Morrissey <zachmor05@gmail.com>
2019-12-16 20:57:38 -0800

    Update client credentials

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,9 @@
 import redis from "redis";
 
-const client: redis.RedisClient = redis.createClient();
+const client: redis.RedisClient = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
 client.set('visits', '0');
 
 export default client;
